76b92cb6a9b23bca360ce68ffefb1697ccd0e3d8
Matthew Johnson <matthew.johnson@intel.com>
2017-03-06 12:14:20 -0800

    Refine OVS command calls
    Also adds deletion of ports and bridges and simplifies the calling
    funcitons by adding internal function to exec the ovs-vsctl call.

--- networking/libsnnet/ovs_bridge.go
+++ networking/libsnnet/ovs_bridge.go
@@ -4,27 +4,32 @@ import (
 	"os/exec"
 )
 
-const (
-	ovs-vsctl := "ovs-vsctl"
-)
 
-func initBridgeCli(id string) error {
+func initBridgeCli(bridgeId string) error {
 	// Example: ovs-vsctl add-br ovs-br1
-	args := []string{"add-br", id}
+	args := []string{"add-br", bridgeId}
 
 	// Execute command
-	if out, err := exec.Command(ovs-vsctl, args...).Output(); err != nil {
+	if err := vsctlCmd(args); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func destroyBridgeCli(id string) error {
+func destroyBridgeCli(bridgeId string) error {
 	// Example: ovs-vsctl del-br ovs-br1
-	args := []string("del-br", id}
+	args := []string{"del-br", bridgeId}
 
-	if out, err := exec.Command(ovs-vsctl, args...).Output(); err != nil {
+	if err := vsctlCmd(args); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func vsctlCmd(args []string) error {
+	if _, err := exec.Command("ovs-vsctl", args...).Output(); err != nil {
 		return err
 	}
 
