7def57fc4409809e1955222771e51ff4fe4f42c8
codymalick <cody.malick@gmail.com>
2017-03-05 17:31:32 -0800

    Added basics for OVS CLI Calls
    Added basic exec calls to shell to manage the OVS managment db.

--- ciao-launcher/network.go
+++ ciao-launcher/network.go
@@ -60,7 +60,8 @@ func initNetworkPhase1() error {
 	cn.NetworkConfig = &libsnnet.NetworkConfig{
 		ManagementNet: mnetList,
 		ComputeNet:    cnetList,
-		Mode:          libsnnet.GreTunnel,
+		//Mode:          libsnnet.GreTunnel,
+		Mode:          OvsGreTunnel,
 	}
 
 	libsnnet.CnMaxAPIConcurrency = 1
