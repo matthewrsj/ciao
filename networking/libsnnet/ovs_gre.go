6333398a4081493d76c3cef727a8c8d4c85a0bd6
Matthew Johnson <matthew.johnson@intel.com>
2017-03-06 12:14:20 -0800

    Refine OVS command calls
    Also adds deletion of ports and bridges and simplifies the calling
    funcitons by adding internal function to exec the ovs-vsctl call.

--- networking/libsnnet/ovs_gre.go
+++ networking/libsnnet/ovs_gre.go
@@ -1,15 +1,44 @@
 package libsnnet
 
 import (
-
+	"fmt"
+	"os/exec"
 )
 
-func addPortCli(id string, portId string) error {
+func addPortInternal(bridgeId string, portId string) error {
 	// Example: ovs-vsctl add-port ovs-br1 endpoint1
 	// Usage: ovs-vsctl add-port <bridge> <port-name>
-	args := []string{"add-port", id, portId}
+	args := []string{"add-port", bridgeId, portId, "--", "set", "interface", portId, "type=internal"}
 
-	if out, err := exec.Command(ovs-vsctl, args...).Output(); err != nil {
+	if err := vsctlCmd(args); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func addGrePort(bridgeId string, portId string, remoteIp string) error {
+	args := []string{"add-port", bridgeId, portId, "--", "set", "interface", portId, "type=gre", fmt.Sprintf("options:remote_ip=%s", remoteIp)}
+
+	if err := vsctlCmd(args); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func delGrePort(bridgeId string, portId string) error {
+	args := []string{"del-port", bridgeId, portId}
+
+	if err := vsctlCmd(args); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func vsctlCmd(args []string) error {
+	if _, err := exec.Command("ovs-vsctl", args...).Output(); err != nil {
 		return err
 	}
 
